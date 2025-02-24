791364b43b643df39938a5f21349505aaaacbd43
Stanislav Antonenko <djak741steam2@mail.ru>
2025-02-24 13:02:54 +0300

    feat(app): bootstrap module

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-spl_autoload_register();
-
-db\Db::init();
+require_once('bootstrap.php');
 header("Cache-Control: no-cache, must-revalidate"); 
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); 
 try {
